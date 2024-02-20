8e188589bf86986a329299dc3048ff12ad571743
Whit Allee <whitallee@gmail.com>
2024-02-19 22:53:53 -0600

    authOptions attempt #5 handler changed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,5 @@ import { authOptions } from "@/lib/utils";
 
 export const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler.handlers.GET;
+export const POST = handler.handlers.POST;
