05dfb06d8b61f4e680f4373925d4e064a6a57a3e
Whit Allee <whitallee@gmail.com>
2024-02-19 20:03:17 -0600

    authOption import on tasks page error fix for real this time?

--- lib/server-actions.ts
+++ lib/server-actions.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '../app/api/auth/[...nextauth]/route';
 import prisma from '@/util/prisma-client';
 import { redirect } from 'next/navigation';
 
